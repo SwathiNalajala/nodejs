5194fa7c8793738f92ca7ded017e8e6e3a0430aa
SwathiNalajala <swathinalajala51@gmail.com>
2017-07-21 10:41:52 +0530

    Update node.js

--- node.js
+++ node.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+var http=require('http');
+
+http.createServer(function (req,res){
+					res.writeHead(200,{'Content-Type':'text/plain')};
+					res.end("Hello world");
+					}).listen(8081);
+console.log("Server running");
