364cdd03f7c9e669f50a14657779c91c378b5c5a
SwathiNalajala <swathinalajala51@gmail.com>
2017-07-24 10:10:50 +0530

    Update nodebuffer.js

--- nodebuffer.js
+++ nodebuffer.js
@@ -1,3 +1,12 @@
 buf = new Buffer(256);  
 len = buf.write("why always 20");  
 console.log("Octets written : "+  len);  
+buf = new Buffer(26);  
+for (var i = 0 ; i < 26 ; i++) {  
+  buf[i] = i + 97;  
+}  
+console.log(buf[1]);
+console.log( buf.toString('ascii'));       // outputs: abcdefghijklmnopqrstuvwxyz  
+console.log( buf.toString('ascii',0,5));   // outputs: abcde  
+console.log( buf.toString('utf8',0,5));    // outputs: abcde  
+console.log( buf.toString(undefined,0,5)); // encoding defaults to 'utf8', outputs abcde
