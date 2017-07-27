f45a415fed204e9ca93487338097ffe35294e6bb
SwathiNalajala <swathinalajala51@gmail.com>
2017-07-27 17:45:55 +0530

    Add files via upload

---
+++ assert.js
@@ -0,0 +1,20 @@
+var assert=require('assert');
+
+const obj1 = {
+  a: {
+    b: 1
+  }
+};
+const obj2 = {
+  a: {
+    b: 2
+  }
+};
+const obj3 = {
+  a: {
+    b: 1
+  }
+};
+const obj4 = Object.create(obj1);
+assert.deepEqual(obj1,obj3);
+assert.deepEqual(obj1,obj2);
